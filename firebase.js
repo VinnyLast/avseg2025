49307a1d55a2e5ed1dcd6deb1f73cc7aba191d4e
Vini <zmisterfire@gmail.com>
2025-11-06 20:40:08 -0300

    Atualização no site da AVSEG firedatabase

--- firebase.js
+++ firebase.js
@@ -1,14 +1,12 @@
 // Importar Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-firestore.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-storage.js";
 
 // Configuração Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDC38KMC9I2twZAA2jY-qfUXsXOLTX0W9Y",
   authDomain: "avseg2025.firebaseapp.com",
   projectId: "avseg2025",
-  storageBucket: "avseg2025.firebasestorage.app",
+  storageBucket: "avseg2025.appspot.com",
   messagingSenderId: "624856699500",
   appId: "1:624856699500:web:028b4a8cdbe56175bb3e49"
 };
